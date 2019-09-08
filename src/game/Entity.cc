0e5138e9d6e91d3d43c72f88f6563e90ea2139d7
Malo ALLEE <malo.allee@gmail.com>
2019-09-08 13:27:05 +0200

    physics working with joint

--- src/game/Entity.cc
+++ src/game/Entity.cc
@@ -12,10 +12,11 @@ namespace game {
           : dynamicsWorld(DynamicsWorld(Vector3(0, -9.81, 0))) {
 
     auto *box = new BoxShape(Vector3(1000, 1, 1000));
-    Transform transform(Vector3(0, 2.5, 0), Quaternion::identity());
+    dynamicsWorld.enableSleeping(false);
+    Transform transform(Vector3(0, 2.5, 0), Quaternion::fromEulerAngles(0, 0, 0.0001));
     body1 = dynamicsWorld.createRigidBody(transform);
     body1->setType(BodyType::DYNAMIC);
-    transform = Transform(Vector3(0, 8.5, 0), Quaternion::identity());
+    transform = Transform(Vector3(0, 8.5, 0), Quaternion::fromEulerAngles(0, 0, 0.0001));
     body2 = dynamicsWorld.createRigidBody(transform);
     body2->setType(BodyType::DYNAMIC);
     transform = Transform(Vector3(0, -1, 0), Quaternion::identity());
@@ -25,7 +26,7 @@ namespace game {
     transform = Transform(Vector3(0, 0, 0), Quaternion::identity());
     proxyShape1 = body1->addCollisionShape(&capsuleShape, transform, 1);
     proxyShape2 = body2->addCollisionShape(&capsuleShape, transform, 1);
-    proxyShape2 = floor->addCollisionShape(box, transform, 1);
+    floor->addCollisionShape(box, transform, 1);
 
     BallAndSocketJointInfo jointInfo(body1, body2, Vector3(0, 5.5, 0));
     joint = dynamic_cast<rp3d::BallAndSocketJoint *>(dynamicsWorld.createJoint(jointInfo));
@@ -34,6 +35,7 @@ namespace game {
   void Entity::update() {
     dynamicsWorld.update(0.0166666);
     std::cout << "body1 " << body1->getTransform().to_string() << std::endl;
-    std::cout << "floor " << floor->getTransform().to_string() << std::endl;
+    std::cout << "body2 " << body2->getTransform().to_string() << std::endl;
+    //std::cout << "floor " << floor->getTransform().to_string() << std::endl;
   }
 }
--- src/main.cc
+++ src/main.cc
@@ -6,13 +6,13 @@ using namespace game;
 int main() {
   Entity entity = Entity();
 
-  for (int i = 0; i < 60 * 5; ++i) {
+  for (int i = 0; i < 60 * 70; ++i) {
     entity.update();
   }
 
   render::Window window;
-  window.init();
-  glfwTerminate();
+//  window.init();
+//  glfwTerminate();
 //  render::Window window;
 //  game::Engine engine;
   return 0;
