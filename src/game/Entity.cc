833d031676571263ffaaa8d2f9930be73bc2b95c
Malo ALLEE <malo.allee@gmail.com>
2019-09-12 23:36:24 +0200

    coding style * + arrays (rigidBody, Shapes, Joints)

--- src/game/Entity.cc
+++ src/game/Entity.cc
@@ -5,37 +5,45 @@ using namespace reactphysics3d;
 
 namespace game {
 
-  CapsuleShape Entity::capsuleShape = CapsuleShape(1, 3);
-  //BoxShape Entity::boxShape;
+  static CapsuleShape* getCapsuleShapeBase() {
+    static CapsuleShape* capsuleShape = nullptr;
+    if (capsuleShape == nullptr)
+      capsuleShape = new CapsuleShape(1, 3);
+    return capsuleShape;
+  }
 
-  Entity::Entity()
-          : dynamicsWorld(DynamicsWorld(Vector3(0, -9.81, 0))) {
+  static BoxShape* getFloorShapeBase() {
+    static BoxShape* floorShape = nullptr;
+    if (floorShape == nullptr)
+      floorShape = new BoxShape(Vector3(1000, 1, 1000));
+    return floorShape;
+  }
 
-    auto *box = new BoxShape(Vector3(1000, 1, 1000));
+  Entity::Entity() : dynamicsWorld(DynamicsWorld(Vector3(0, -9.81, 0))) {
+
     dynamicsWorld.enableSleeping(false);
     Transform transform(Vector3(0, 2.5, 0), Quaternion::fromEulerAngles(0, 0, 0.0001));
-    body1 = dynamicsWorld.createRigidBody(transform);
-    body1->setType(BodyType::DYNAMIC);
+    rigidBodies[0] = dynamicsWorld.createRigidBody(transform);
+    rigidBodies[0]->setType(BodyType::DYNAMIC);
     transform = Transform(Vector3(0, 8.5, 0), Quaternion::fromEulerAngles(0, 0, 0.0001));
-    body2 = dynamicsWorld.createRigidBody(transform);
-    body2->setType(BodyType::DYNAMIC);
+    rigidBodies[1] = dynamicsWorld.createRigidBody(transform);
+    rigidBodies[1]->setType(BodyType::DYNAMIC);
     transform = Transform(Vector3(0, -1, 0), Quaternion::identity());
     floor = dynamicsWorld.createRigidBody(transform);
     floor->setType(BodyType::STATIC);
 
     transform = Transform(Vector3(0, 0, 0), Quaternion::identity());
-    proxyShape1 = body1->addCollisionShape(&capsuleShape, transform, 1);
-    proxyShape2 = body2->addCollisionShape(&capsuleShape, transform, 1);
-    floor->addCollisionShape(box, transform, 1);
+    proxyShapes[0] = rigidBodies[0]->addCollisionShape(getCapsuleShapeBase(), transform, 1);
+    proxyShapes[1] = rigidBodies[1]->addCollisionShape(getCapsuleShapeBase(), transform, 1);
+    floor->addCollisionShape(getFloorShapeBase(), transform, 1);
 
-    BallAndSocketJointInfo jointInfo(body1, body2, Vector3(0, 5.5, 0));
-    joint = dynamic_cast<rp3d::BallAndSocketJoint *>(dynamicsWorld.createJoint(jointInfo));
+    BallAndSocketJointInfo jointInfo(rigidBodies[0], rigidBodies[1], Vector3(0, 5.5, 0));
+    joints[0] = dynamic_cast<rp3d::BallAndSocketJoint*>(dynamicsWorld.createJoint(jointInfo));
   }
 
   void Entity::update() {
     dynamicsWorld.update(0.0166666);
-    std::cout << "body1 " << body1->getTransform().to_string() << std::endl;
-    std::cout << "body2 " << body2->getTransform().to_string() << std::endl;
-    //std::cout << "floor " << floor->getTransform().to_string() << std::endl;
+    std::cout << "body1 " << rigidBodies[0]->getTransform().to_string() << std::endl;
+    std::cout << "body2 " << rigidBodies[1]->getTransform().to_string() << std::endl;
   }
 }
--- src/game/Entity.hh
+++ src/game/Entity.hh
@@ -7,18 +7,16 @@ namespace game {
   class Entity {
   public:
     Entity();
+
     void update();
 
   private:
-    reactphysics3d::DynamicsWorld dynamicsWorld;
-    reactphysics3d::RigidBody *body1;
-    reactphysics3d::RigidBody *body2;
-    reactphysics3d::RigidBody *floor;
-    reactphysics3d::ProxyShape *proxyShape1;
-    reactphysics3d::ProxyShape *proxyShape2;
-    reactphysics3d::BallAndSocketJoint *joint;
+    static const int nbRigidBodies = 4;
 
-    static reactphysics3d::CapsuleShape capsuleShape;
-    static reactphysics3d::BoxShape boxShape;
+    reactphysics3d::DynamicsWorld dynamicsWorld;
+    reactphysics3d::RigidBody* floor;
+    reactphysics3d::RigidBody* rigidBodies[nbRigidBodies];
+    reactphysics3d::ProxyShape* proxyShapes[nbRigidBodies];
+    reactphysics3d::BallAndSocketJoint* joints[nbRigidBodies - 1];
   };
 }
